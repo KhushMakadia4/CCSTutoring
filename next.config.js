8655beaea9247609dd7bc19c589d40d46dbbdb9f
Khush Makadia <71839602+KhushMakadia4@users.noreply.github.com>
2022-01-31 09:04:38 -0600

    tailwind nav

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: false,
+  images: {
+    domains: ['tailwindui.com', 'images.unsplash.com','www.w3.org'],
+    format: ['image/webp']
+  }
 };
