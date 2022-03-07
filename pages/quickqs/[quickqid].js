78494130d01571620d80559eb88f03afbc5342f0
Khush Makadia <71839602+KhushMakadia4@users.noreply.github.com>
2022-03-07 09:05:05 -0600

    comment system updating working

--- pages/quickqs/[quickqid].js
+++ pages/quickqs/[quickqid].js
@@ -1,18 +1,29 @@
-import { addDoc, collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, getDocs, onSnapshot } from "firebase/firestore";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../../utils/Firebase";
 
 export default function QuickQ({ qdata }) {
   const router = useRouter();
   const { quickqid } = router.query;
 
-  const [qdState, setQDState] = useState(qdata)
+  // const [qdata, ] = useState(qdata)
+  const [comments, setComments] = useState([])
+
+  useEffect(()=>{
+    // onSnapshot()
+    const tempComm = []
+    qdata.comments.forEach((comment)=>{
+      tempComm.push(comment)
+    })
+    setComments(tempComm)
+    // console.log("useEffect", comments[1])
+  }, [])
 
   return (
     <div className="flex flex-col">
-      {qdState === undefined ? (
+      {qdata === undefined ? (
         <h1>
           No page for{" "}
           {process.env.NEXT_PUBLIC_HOST.substring(
@@ -39,7 +50,7 @@ export default function QuickQ({ qdata }) {
           <h3>{qdata.description}</h3>
           <h4>{qdata.createdBy}</h4>
           <h5>resolved: {qdata.resolved.toString()}</h5>
-          {qdState.resolved.toString()==="false" ? (
+          {qdata.resolved.toString()==="false" ? (
             <section id="app">
               <div className="max-w-2xl flex my-7 bg-white rounded-lg">
                 <input
@@ -50,7 +61,6 @@ export default function QuickQ({ qdata }) {
                   placeholder="Write a comment"
                 ></input>
                 <button onClick={async () => {
-                  console.log("comment adding started qdstate: ", qdState)
                     await addDoc(
                       collection(
                         db,
@@ -63,26 +73,28 @@ export default function QuickQ({ qdata }) {
                           .value.toString(),
                       }
                     ).then(async () => {
-                      console.log("doc added qdata: ", qdata);
-                      console.log("doc added qdState: ", qdState);
-                      qdata.comments = [];
-                      setQDState(qdata)
-                      console.log("qdata comments cleared qdata: ", qdata)
-                      console.log("qdata comments cleared qdstate: ", qdState);
-                      await getDocs(
-                        collection(
-                          db,
-                          "quickqs/" + quickqid.toString() + "/comments"
-                        )
-                      ).then((comSnap) => {
-                        if (comSnap.size > 0) {
-                          comSnap.docs.forEach((comment) => {
-                            qdata.comments.push(comment.data());
-                          });
-                        }
-                      });
-                      setQDState(qdata)
-                      console.log("comments refilled qdState: ", qdState);
+                      setComments(comments=>[ {writtenBy: "Khushjeet Makadamian", text: document
+                      .getElementById("commentTBox") //? Add server timestamp to code and arrange code by that!
+                      .value.toString()}, ...comments])//!Change later to signed in user name
+
+                      // console.log("doc added qdata: ", qdata);
+                      // console.log("doc added qdata: ", qdata);
+                      // qdata.comments = [];
+                      // console.log("qdata comments cleared qdata: ", qdata)
+                      // console.log("qdata comments cleared qdata: ", qdata);
+                      // await getDocs(
+                      //   collection(
+                      //     db,
+                      //     "quickqs/" + quickqid.toString() + "/comments"
+                      //   )
+                      // ).then((comSnap) => {
+                      //   if (comSnap.size > 0) {
+                      //     comSnap.docs.forEach((comment) => {
+                      //       qdata.comments.push(comment.data());
+                      //     });
+                      //   }
+                      // });
+                      // console.log("comments refilled qdata: ", qdata);
                     });
                   }}
                   className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white">
@@ -93,7 +105,7 @@ export default function QuickQ({ qdata }) {
           ) : (
             <></>
           )}
-          {qdState.comments.map((comment, i)=> (
+          {comments.map((comment, i)=> (
             <div>
               <h3>{comment.writtenBy}: {comment.text}</h3>
               <div className="h-20 border-t-8 border-black border-double ml-48 mr-48" />
