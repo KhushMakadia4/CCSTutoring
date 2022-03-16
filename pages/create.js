68e9b760e5bd95139fb9017bc0eb5421d43ce397
Khush Makadia <71839602+KhushMakadia4@users.noreply.github.com>
2022-03-16 09:04:05 -0500

    createpost started

--- pages/create.js
+++ pages/create.js
@@ -1,11 +1,26 @@
-
-
-
 export default function Create() {
+  return (
+    <div>
 
-return (
-<div>
-    <h1>create a post!</h1>
-</div>
-)
+        <h1>CREATE A POST</h1>
+        <hr></hr>
+        <br></br>
+      <div className="mb-3 pt-0">
+        <input
+          id="dayNum"
+          type="text"
+          placeholder="Title"
+          className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <input
+          id="dayName"
+          type="text"
+          placeholder="Description"
+          className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+        />
+      </div>
+    </div>
+  );
 }
